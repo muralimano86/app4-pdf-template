01545409254769a0c2aed5465ac62382c8fb77ab
murali <muralimano86@gmail.com>
2024-02-09 20:17:52 +0000

    Using csv file, add multiple pages

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fpdf import FPDF
+import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hi There!", ln=1, border=1, align="L")
-pdf.cell(w=0, h=12, txt="Hi You!", ln=1, border=0, align="L")
-
-pdf.add_page()
-
-pdf.cell(w=0, h=12, txt="Hi there!", ln=1, border=1, align="M")
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, border=0, align="L")
+    pdf.line(10, 21, 200, 21)
 
 pdf.output("output.pdf")
